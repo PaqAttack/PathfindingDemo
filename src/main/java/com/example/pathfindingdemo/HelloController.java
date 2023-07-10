fed787f19317750903ea2d9f747c6407d7a916b9
PaqAttack <94795437+PaqAttack@users.noreply.github.com>
2023-07-10 13:54:06 -0700

    Setup basic project

--- src/main/java/com/example/pathfindingdemo/HelloController.java
+++ src/main/java/com/example/pathfindingdemo/HelloController.java
@@ -1,14 +1,31 @@
 package com.example.pathfindingdemo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private TextField hSize;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField vSize;
+    @FXML
+    private Canvas myCanvas;
+
+    int height;
+    int width;
+
+    public void generateGrid(ActionEvent event) {
+        height = Integer.parseInt(vSize.getText());
+        width = Integer.parseInt(hSize.getText());
+
+        System.out.println(height + width);
+
     }
+
+
+
 }
